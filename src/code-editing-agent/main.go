1babf20c96209849cf5225d1605c88f31403d8b2
Denis Borchev <askbow@users.noreply.github.com>
2026-01-18 13:02:31 +0100

    make conversation loop work

--- src/code-editing-agent/main.go
+++ src/code-editing-agent/main.go
@@ -5,14 +5,17 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"text/scanner"
 
 	"github.com/anthropics/anthropic-sdk-go"
+	"github.com/anthropics/anthropic-sdk-go/option"
 )
 
 func main() {
-    client := anthropic.NewClient(os.Getenv("ANTHROPIC_API_KEY"))
-
+	client := anthropic.NewClient(
+		// setting variables is scoped to User or System on windows
+		// it is astonishing there is no `export` like in Linux
+		option.WithAPIKey(os.Getenv("ANTHROPIC_API_KEY")), // defaults to os.LookupEnv("ANTHROPIC_API_KEY")
+	)
 	scanner := bufio.NewScanner(os.Stdin)
 
 	getUserMessage := func() (string, bool) {
@@ -29,15 +32,55 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		// no os.Exit(1)
 	}
+}
 
 type Agent struct {
-	client *anthropic.Client
+	client         *anthropic.Client
 	getUserMessage func() (string, bool)
 }
 
 func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
 	return &Agent{
-		client: client,
+		client:         client,
 		getUserMessage: getUserMessage,
 	}
+}
+
+func (a *Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (*anthropic.Message, error) {
+	message, err := a.client.Messages.New(ctx, anthropic.MessageNewParams{
+		Model:     anthropic.ModelClaudeSonnet4_5, //https://github.com/anthropics/anthropic-sdk-go/blob/main/message.go#L2081
+		MaxTokens: int64(1024),
+		Messages:  conversation,
+	})
+	return message, err
+}
+
+func (a *Agent) Run(ctx context.Context) error {
+	// main interaction loop
+	conversation := []anthropic.MessageParam{}
+	fmt.Println("Chat with Agent (use 'ctrl-c' to quit)")
+
+	for {
+		fmt.Print("\u001b[94mYou\u001b[0m: ")
+		userInputRaw, ok := a.getUserMessage()
+		if !ok {
+			break
+		}
+
+		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInputRaw))
+		conversation = append(conversation, userMessage)
+
+		message, err := a.runInference(ctx, conversation)
+		if err != nil {
+			return err
+		}
+		conversation = append(conversation, message.ToParam())
+		for _, content := range message.Content {
+			switch content.Type {
+			case "text":
+				fmt.Printf("\u001b[93mAgent\u001b[0m: %s\n", content.Text)
+			}
+		}
+	}
+	return nil
 }
